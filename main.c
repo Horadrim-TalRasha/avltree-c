e927cc9504146d04666a070722ee780f6b1588e7
Tal Rasha <524948250@qq.com>
2023-05-22 14:13:52 +0000

    first verification & use ubuntu 18.04

--- main.c
+++ main.c
@@ -1,8 +1,20 @@
-#include "havltree.h"
 #include <stdio.h>
+#include <assert.h>
+
+#include "havltree.h"
+
 
 int main(int argc, char ** argv)
 {
+    HAVLTree * newbie_test_avl_tree = init_avl_tree();
+    if (newbie_test_avl_tree != NULL)
+    {
+        assert(newbie_test_avl_tree->_root == NULL);
+    }
+    else
+    {
+        printf("memory is running out.\n");
+    }
     printf("All verification passed.\n");
     return 0;
 }
